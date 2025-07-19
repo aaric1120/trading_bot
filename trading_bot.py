355cfe339d94c3caede6784247575b605aec8076
chinhanchen <aaric1120@hotmail.com>
2025-07-18 23:30:42 -0400

    updated so the code can run 24/7

--- trading_bot.py
+++ trading_bot.py
@@ -14,49 +14,48 @@ def main():
     POST_MIDNIGHT = dt.time(0, 0, 0)
     CURR_DAY = get_current_date()
 
-    if  MARKET_CLOSE_TIME < dt.datetime.now().time() < PRE_MIDNIGHT \
-            or POST_MIDNIGHT < dt.datetime.now().time() < MARKET_OPEN_TIME:
-        print("The Market hasn't opened yet...")
-        sleep_sec = get_seconds(9,45,0) # PARAM
-
-        # Checks the day of the week to calculate the time to wait
-        if (CURR_DAY == 4 and MARKET_CLOSE_TIME < dt.datetime.now().time() < PRE_MIDNIGHT) or CURR_DAY > 4:
-            sleep_sec += 86400 * (6 - CURR_DAY)
-
-        print(f"Sleeping for {int(sleep_sec/3600)} Hours "
-              f"and {int((sleep_sec % 3600) / 60)} Minutes until Marktet opens...")
-        tm.sleep(sleep_sec)
-
-    # variables that track the process
-    stock_list = []
-    stock_dict = set()
-    process_list = []
-
-    # Main loop to add new stocks every 15 minutes...
     while True:
-        # Check close time for market
-        if dt.datetime.now().time() > MARKET_CLOSE_TIME:
-            print("The Market has closed...")
-            break
-        else:
-            # Check if the process has finished
-            for i in range(len(stock_list)):
-                if (stock_list[i] is not None):
-                    if process_list[i].poll() is not None:
-                        stock_dict.remove(stock_list[i])
-                        stock_list[i] = None
+        if MARKET_CLOSE_TIME < dt.datetime.now().time() < PRE_MIDNIGHT \
+                or POST_MIDNIGHT < dt.datetime.now().time() < MARKET_OPEN_TIME:
+            print("The Market hasn't opened yet...")
+            sleep_sec = get_seconds(9,45,0) # PARAM
 
-            # Don't get new stock after deadline
-            if dt.datetime.now().time() < MARKET_DEADLINE:
-                # Get new stocks to add...
-                print("Adding new stocks to the list...")
-                get_stock_start(stock_list, stock_dict, process_list)
-                print(f"The Current active stocks are {stock_dict}")
+            # Checks the day of the week to calculate the time to wait
+            if (CURR_DAY == 4 and MARKET_CLOSE_TIME < dt.datetime.now().time() < PRE_MIDNIGHT) or CURR_DAY > 4:
+                sleep_sec += 86400 * (6 - CURR_DAY)
 
-            print("Sleeping for 15 minutes until next stock check...")
-            tm.sleep(900)  # PARAM
+            print(f"Sleeping for {int(sleep_sec/3600)} Hours "
+                  f"and {int((sleep_sec % 3600) / 60)} Minutes until Marktet opens...")
+            tm.sleep(sleep_sec)
 
-    return
+        # variables that track the process
+        stock_list = []
+        stock_dict = set()
+        process_list = []
+
+        # Main loop to add new stocks every 15 minutes...
+        while True:
+            # Check close time for market
+            if dt.datetime.now().time() > MARKET_CLOSE_TIME:
+                print("The Market has closed...")
+                break
+            else:
+                # Check if the process has finished
+                for i in range(len(stock_list)):
+                    if (stock_list[i] is not None):
+                        if process_list[i].poll() is not None:
+                            stock_dict.remove(stock_list[i])
+                            stock_list[i] = None
+
+                # Don't get new stock after deadline
+                if dt.datetime.now().time() < MARKET_DEADLINE:
+                    # Get new stocks to add...
+                    print("Adding new stocks to the list...")
+                    get_stock_start(stock_list, stock_dict, process_list)
+                    print(f"The Current active stocks are {stock_dict}")
+
+                print("Sleeping for 15 minutes until next stock check...")
+                tm.sleep(900)  # PARAM
 
 
 if __name__ == "__main__":
