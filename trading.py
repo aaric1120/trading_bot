81033fc6ab6f2126d884ad2d1ef122cf7ddb39eb
chinhanchen <aaric1120@hotmail.com>
2025-07-21 12:31:33 -0400

    updated stop loss algo after taking profit

--- trading.py
+++ trading.py
@@ -164,9 +164,9 @@ class BaseTrade:
                         return
 
                     # Updated the take profit price
-                    init_price = take_profit
-                    stop_loss = round(take_profit * self.param["stop_loss"], 2)
+                    stop_loss = round(take_profit, 2)
                     take_profit = round(take_profit * self.param["take_profit"], 2)
+                    init_price = round((take_profit + stop_loss) / 2, 2)
                     print(f"the updated take profit price is {take_profit}, the updated stop loss price is {stop_loss}")
                     logging.info(f"the updated take profit price is {take_profit}, the updated stop loss price is {stop_loss}")
 
